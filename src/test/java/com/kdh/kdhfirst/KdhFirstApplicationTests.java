8f725d1c5fce22eb56f74c4bb027e21dd708956f
bearclaw0731 <bearclaw@naver.com>
2018-07-06 14:40:44 +0900

    프로젝트 생성

---
+++ src/test/java/com/kdh/kdhfirst/KdhFirstApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kdh.kdhfirst;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KdhFirstApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
